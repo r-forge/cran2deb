d5bdf640ef699d663fee4a16a489dfb57bae0d4b
blundellc (charles blundell) <blundellc@gmail.com>
2008-09-13 13:25:59 +0000

    gsoc_final
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cran2deb@119 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tags/gsoc_final/R/zzz.R
@@ -0,0 +1,25 @@
+.First.lib <- function(libname, pkgname) {
+    global <- function(name,value) assign(name,value,envir=.GlobalEnv)
+    global("maintainer", 'cran2deb autobuild <cran2deb@example.org>')
+    global("root", system.file(package='cran2deb'))
+    global("cache_root", '/var/cache/cran2deb')
+    global("pbuilder_results", '/var/cache/cran2deb/results')
+    global("pbuilder_config", file.path(root,'etc/pbuilderrc'))
+    global("dput_config", file.path(root,'etc/dput.cf'))
+    global("dinstall_config", file.path(root,'etc/mini-dinstall.conf'))
+    global("dinstall_archive", file.path(root,'var/archive'))
+    global("r_depend_fields", c('Depends','Imports')) # Suggests, Enhances
+    # git_revision {
+    global("git_revision","b79d88001537df06dac28c27be3d4887ca2d9238")
+    # git_revision }
+    global("changesfile", function(srcname,version='*') {
+        return(file.path(pbuilder_results
+                        ,paste(srcname,'_',version,'_'
+                              ,host_arch(),'.changes',sep='')))
+    })
+
+    cache <- file.path(cache_root,'cache.rda')
+    if (file.exists(cache)) {
+        load(cache,envir=.GlobalEnv)
+    }
+}
